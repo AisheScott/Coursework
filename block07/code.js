227b8153222f35eb050a2d2fbb0a13539d202b7d
AISHE Scott <scott.aishe@hexagon.com>
2024-12-10 21:39:37 -0500

    pest

--- block07/code.js
+++ block07/code.js
@@ -1,5 +1,5 @@
 /**
- * 1 Create a string for user "You have recived this message because you have been chosen to open an important vault"
+1 Create a string for user "You have recived this message because you have been chosen to open an important vault"
 2 assign 3 variables Each variable contains the corresponding result of calculation using a unique arithmetic operator(+-). Each must equal one of the three codes in the combination. 10-40-39
 3 add comments thoughout code to explain each step
 4 Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
